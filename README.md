20a9c4277bf44523e8e1bc19e82deaba61034ee3
Jared Thomas <jaredthomas@Jareds-Air.home>
2020-04-26 11:32:55 -0400

    added form

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 https://github.com/IgorHalfeld/vue-2-course
 https://github.com/vuejs/vue-router/tree/dev/examples
 https://vuejs.org/v2/cookbook/using-axios-to-consume-apis.html
+https://auralinna.blog/post/2018/how-to-build-a-complete-form-with-vue-js/
--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -1,5 +1,33 @@
-<template>
-  <div>
-    <p>post page</p>
-  </div>
+<template lang="html">
+  <form id="postForm" @submit.prevent="processForm">
+    <label>Title</label>
+    <br></br>
+    <input type="text" v-model="postTitle">
+    <br></br>
+    <label>Question</label>
+    <br></br>
+    <textarea type="text" v-model="postBody"></textarea>
+    <br></br>
+    <input type="submit"/>
+  </form>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      postTitle: '',
+      postBody: ''
+    }
+  }),
+  methods: {
+    processForm () {
+      console.log(this.postTitle);
+      console.log(this.postBody);
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
