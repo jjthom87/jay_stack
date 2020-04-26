04cdd94a7e989eb9cf7d2ab3112424e61ac6c121
Jared Thomas <jaredthomas@Jareds-Air.home>
2020-04-26 18:16:40 -0400

    added post

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -1,29 +1,37 @@
 <template lang="html">
   <form id="postForm" @submit.prevent="processForm">
     <label>Title</label>
-    <br></br>
-    <input type="text" v-model="postTitle">
-    <br></br>
+    <br>
+    <input type="text" v-model="articleTitle">
+    <br>
     <label>Question</label>
-    <br></br>
-    <textarea type="text" v-model="postBody"></textarea>
-    <br></br>
+    <br>
+    <textarea type="text" v-model="articleBody"></textarea>
+    <br>
     <input type="submit"/>
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     form: {
-      postTitle: '',
-      postBody: ''
+      articleTitle: '',
+      articleBody: ''
     }
   }),
   methods: {
     processForm () {
-      console.log(this.postTitle);
-      console.log(this.postBody);
+      axios.post(`http://localhost:3000/v1/api/article`, {
+        articleTitle: this.articleTitle,
+        articleBody: this.articleBody
+      })
+      .then(response => {})
+      .catch(e => {
+        console.log(e);
+      })
     }
   }
 }
