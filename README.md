a1a3292c608f814ce84ff0134f8a19058c0d9653
Jared Thomas <jaredthomas@Jareds-Air.home>
2020-04-30 11:35:39 -0400

    added bootstrap, better get some shit done later

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ https://github.com/IgorHalfeld/vue-2-course
 https://alligator.io/vuejs/rest-api-axios/
 https://auralinna.blog/post/2018/how-to-build-a-complete-form-with-vue-js/
 https://appdividend.com/2018/12/28/vue-router-tutorial-with-example-how-to-use-routing-in-vuejs/
+https://vuematerial.io/
--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -1,17 +1,28 @@
-<template lang="html">
-  <md-card>
+<template>
+  <div class="d-flex align-content-center">
     <form class="md-layout" id="postForm" @submit.prevent="processForm">
-      <label>Title</label>
-      <br>
-      <input type="text" v-model="articleTitle">
-      <br>
-      <label>Question</label>
-      <br>
-      <textarea type="text" v-model="articleBody"></textarea>
-      <br>
-      <input type="submit"/>
+      <md-card class="md-layout-item md-size-50 md-small-size-50">
+        <md-card-header>
+          <div class="md-title">Post Question</div>
+        </md-card-header>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-50">
+              <label for="title">Title</label>
+              <input type="text" v-model="articleTitle"/>
+            </div>
+            <div class="md-layout-item md-small-size-50">
+              <label for="question">Question</label>
+              <textarea type="text" v-model="articleBody"></textarea>
+            </div>
+          </div>
+        </md-card-content>
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Create Post</md-button>
+        </md-card-actions>
+      </md-card>
     </form>
-  </md-card>
+  </div>
 </template>
 
 <script>
